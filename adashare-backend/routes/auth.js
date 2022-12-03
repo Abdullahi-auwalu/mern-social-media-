53c9540ea07de19326a4ebc03d1dc708e78d3309
Abdullahi-auwalu <awaluabdullahi07@gmail.com>
2022-12-03 11:22:09 +0100

    first commit

---
+++ adashare-backend/routes/auth.js
@@ -0,0 +1,15 @@
+const router = require("express").Router();
+const User = require("../models/User");
+
+//REGISTER
+router.get("/register", async (req, res) => {
+  const user = await new User({
+    username: "Khamis",
+    email: "khamis@gmail.com",
+    password: "123456",
+  });
+  await user.save();
+  res.send("ok");
+});
+
+module.exports = router;
