20b235ac5fe6bddce40e90f861837f81e1f4e4b9
Abdullahi-auwalu <awaluabdullahi07@gmail.com>
2022-12-03 15:31:15 +0100

    add login

--- adashare-backend/routes/auth.js
+++ adashare-backend/routes/auth.js
@@ -1,15 +1,45 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const bcrypt = require("bcrypt");
+const { application } = require("express");
 
 //REGISTER
-router.get("/register", async (req, res) => {
-  const user = await new User({
-    username: "Khamis",
-    email: "khamis@gmail.com",
-    password: "123456",
-  });
-  await user.save();
-  res.send("ok");
+router.post("/register", async (req, res) => {
+  try {
+    //Hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(req.body.password, salt);
+
+    // Create new user
+    const newUser = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hashPassword,
+    });
+
+    // Save user and  response
+    const user = await newUser.save();
+    res.status(200).json(user);
+  } catch (err) {
+    console.log(err);
+  }
 });
 
+// LOGIN
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    !user && res.status(404).json("user not found");
+
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    !validPassword && res.status(400).json("Wrong password");
+
+    res.status(200).json(user);
+  } catch (err) {
+    console.log(err);
+  }
+});
 module.exports = router;
