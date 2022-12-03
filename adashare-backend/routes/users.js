3f86159b5c66af0459222d7b94d08b04a5ed9e99
Abdullahi-auwalu <awaluabdullahi07@gmail.com>
2022-12-03 20:43:44 +0100

    add update password

--- adashare-backend/routes/users.js
+++ adashare-backend/routes/users.js
@@ -1,7 +1,26 @@
 const router = require("express").Router();
+const user = require("../models/User");
+const bcrypt = require("bcrypt");
 
-router.get("/", (req, res) => {
-  res.send("hey it's route");
+// UPDATE USER
+router.put("/:id", async (req, res) => {
+  if (req.body.userid === req.params.id || req.user.isAdmin) {
+    if (req.body.password) {
+      try {
+        const salt = await bcrypt.genSalt(10);
+        req.body.password = await bcrypt.hash(req.body.password, salt);
+      } catch (err) {
+        return res.status(500).json(err);
+      }
+    }
+  } else {
+    return res.status(403).json("You can only update your account!");
+  }
 });
+
+//DELETE USER
+//GET A USER
+//FOLLOW A USER
+//UNFOLLOW A USER
 
 module.exports = router;
