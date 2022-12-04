5a1cc164e83840ae40d90d1e06c2de59a50d75f3
Abdullahi-auwalu <awaluabdullahi07@gmail.com>
2022-12-04 22:32:04 +0100

    add follow and unfollow

--- adashare-backend/routes/users.js
+++ adashare-backend/routes/users.js
@@ -50,7 +50,49 @@ router.get("/:id", async (req, res) => {
     res.status(200).json(other);
   } catch (err) {}
 });
+
 //FOLLOW A USER
+router.put("/:id/follow", async (req, res) => {
+  if (req.body.userId !== req.params.id) {
+    try {
+      const user = await User.findById(req.params.id);
+      const currentUser = await User.findById(req.body.userId);
+
+      if (!user.followers.includes(req.body.userId)) {
+        await user.updateOne({ $push: { followers: req.body.userId } });
+        await currentUser.updateOne({ $push: { followings: req.params.id } });
+        res.status(200).json("User has been followed");
+      } else {
+        res.status(403).json("You already follow this user!");
+      }
+    } catch {
+      res.status(500).json("Unable to follow");
+    }
+  } else {
+    res.status(403).json("You can't follow yourself!");
+  }
+});
+
 //UNFOLLOW A USER
+router.put("/:id/unfollow", async (req, res) => {
+  if (req.body.userId !== req.params.id) {
+    try {
+      const user = await User.findById(req.params.id);
+      const currentUser = await User.findById(req.body.userId);
+
+      if (user.followers.includes(req.body.userId)) {
+        await user.updateOne({ $pull: { followers: req.body.userId } });
+        await currentUser.updateOne({ $pull: { followings: req.params.id } });
+        res.status(200).json("User has been unfollowed");
+      } else {
+        res.status(403).json("You already unfollow this user!");
+      }
+    } catch {
+      res.status(500).json("Unable to unfollow");
+    }
+  } else {
+    res.status(403).json("You dont't unfollow yourself!");
+  }
+});
 
 module.exports = router;
