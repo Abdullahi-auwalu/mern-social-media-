9d99034ec3d566ca2d5cf763ffe745d5a8e46a5b
Abdullahi-auwalu <awaluabdullahi07@gmail.com>
2022-12-04 17:49:17 +0100

    add get a user

--- adashare-backend/routes/users.js
+++ adashare-backend/routes/users.js
@@ -43,6 +43,13 @@ router.delete("/:id", async (req, res) => {
 });
 
 //GET A USER
+router.get("/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    const { password, updatedAt, ...other } = user._doc;
+    res.status(200).json(other);
+  } catch (err) {}
+});
 //FOLLOW A USER
 //UNFOLLOW A USER
 
