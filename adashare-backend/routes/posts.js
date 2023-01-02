9612af92269143306e8f81575a5354c419dd2f88
Abdullahi-auwalu <awaluabdullahi07@gmail.com>
2023-01-02 06:01:48 +0100

    delete post

--- adashare-backend/routes/posts.js
+++ adashare-backend/routes/posts.js
@@ -30,6 +30,21 @@ router.put("/:id", async (req, res) => {
 });
 
 //delete a post
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.userId === req.body.userId) {
+      await post.deleteOne();
+      res.status(200).json("The post has been deleted");
+    } else {
+      res.status(403).json("You can only delete your post");
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 //like a post
 //get a post
 //get timeline posts
