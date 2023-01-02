01bda2f1abcd9e74fdd9ae04bacad1f85528cd99
Abdullahi-auwalu <awaluabdullahi07@gmail.com>
2023-01-02 06:11:33 +0100

    like post

--- adashare-backend/routes/posts.js
+++ adashare-backend/routes/posts.js
@@ -46,6 +46,19 @@ router.delete("/:id", async (req, res) => {
 });
 
 //like a post
+
+router.put("/:id/like", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post.likes.includes(res.body.userId)) {
+      await post.updateOne({ $push: { likes: req.body.userId } });
+      res.status(200).json("The post has been liked");
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 //get a post
 //get timeline posts
 
