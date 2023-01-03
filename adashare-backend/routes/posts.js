32b54c69b6edd4d8a13c0c1161bbce89d40181fd
Abdullahi-auwalu <awaluabdullahi07@gmail.com>
2023-01-03 22:18:45 +0100

    client

--- adashare-backend/routes/posts.js
+++ adashare-backend/routes/posts.js
@@ -9,7 +9,7 @@ router.post("/", async (req, res) => {
     const savedPost = await newPost.save();
     res.status(200).json(savedPost);
   } catch {
-    res.statu(500).json(err);
+    res.status(500).json(err);
   }
 });
 
@@ -75,4 +75,21 @@ router.get("/:id", async (req, res) => {
 
 //get timeline posts
 
+router.get("/timeline/all", async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.body.userId);
+    const userPosts = await Post.find({ userId: currentUser._id });
+    const friendPosts = await Promise.all(
+      currentUser.following.map((friendId) => {
+        return Post.find({ userId: friendId });
+      })
+    );
+    res.json(userPosts.concat(...friendPosts));
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
+
+// Google Font to use
